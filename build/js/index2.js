f8ad0d7a1d5edabf52ce2096cac71738c159c6cc
buccaneer33 <buccaneer33@inbox.ru>
2018-04-10 21:52:01 +0400

    21-51 10-04-18 open form, close form, clear form

--- build/js/index2.js
+++ build/js/index2.js
@@ -1,44 +1,44 @@
-window.onload = function () {
+window.onload = function () 
 
-		var clearInputs = function(){
-		var Inputs = document.getElementsByClassName("add-book__input");
+	var clearInputs = function(){
+		/*var Inputs = document.getElementsByClassName("add-book__input");
 
 		for(var i = 0; i < Inputs.length; i++){
 			if(Inputs[i].type == 'text'){
 				Inputs[i].value = '';
 			}
-		};
-		var yearInput = document.getElementsByClassName("add-book__year");
+
+		};*/
+		/*var yearInput = document.getElementsByClassName("add-book__year");
 		for(var i = 0; i < yearInput.length; i++){
 			if(yearInput[i].type == 'text'){
 				yearInput[i].value = '';
 			}
-		};
-	}
-  var openForm = function() {
 
+		};*/
+	}
+
+  var openForm = function() {
+      
 		document.getElementById('add-book--background').classList.remove("visible--disable");
 		document.getElementById('add-book').classList.remove("visible--disable");
 
     }
   var closeForm = function() {
-
+		
 		document.getElementById('add-book--background').classList.add("visible--disable");
 		document.getElementById('add-book').classList.add("visible--disable");
 
     }
 
 	document.getElementById('button__adding-form--open1').onclick = function() {
-	clearInputs();
-	openForm();
+	openForm()
 	}
 	document.getElementById('button__adding-form--open2').onclick = function() {
-	clearInputs();
-	openForm();
+	openForm()
 	}
 	document.getElementById('button--close').onclick = function() {
-	clearInputs();
-	closeForm();
+	closeForm()
 	}
-
+	
 }
