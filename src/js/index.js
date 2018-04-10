9ab67b49c0a8e6074779347eb76e43e9e63861e2
buccaneer33 <buccaneer33@inbox.ru>
2018-04-10 15:43:23 +0400

    15-43 10-04-18

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // --- Библиотеки
 //= jquery-3.2.1.min.js
+window.onload = function () {
+
+    //получаем идентификатор элемента
+    var openForm1 = document.getElementById('button__adding-form--open1');
+    var openForm2 = document.getElementById('button__adding-form--open2');
+
+openForm1.onClick = openForm();
+
+    //вешаем на него событие
+  var openForm = function() {
+      alert('Click');
+    }
+}
